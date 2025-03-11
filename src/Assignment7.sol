cb252975ec0f16213d33effe402682c7b1147c87
Sherief Elhamy <sherief.elhamy7@gmail.com>
2025-03-11 07:28:56 +0200

    :fire: feat: solved

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,18 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
+    address private owner;
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+        
+    }
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address to, uint256 amount) external onlyOwner {
         // Fill in the logic
         // call _mint function with to and amount params
+        _mint(to, amount);
     }
 }
